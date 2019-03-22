162b5570a0f589e431a40f95814aee54860183f4
Gabriel Julio <gabrieldasilvajulio@gmail.com>
2019-03-21 22:07:26 -0300

    Commit inicial com arquivos essenciais do projeto
    - Criação das classes já pensadas
    OBS: Classes ainda não totalmente implementadas

---
+++ 18196_18204_Projeto1ED/18196_18204_Projeto1ED/Celula.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+public class Celula : IComparable<Celula>
+{
+    Celula direita;
+    Celula abaixo;
+    int linha, coluna;
+    double valor;
+
+    public Celula(Celula d, Celula ab, int li, int col, double val)
+    {
+        if(d != null && ab != null)
+        {
+            this.Direita = d;
+            this.Abaixo = ab;
+        }        
+        this.Linha = li;
+        this.Coluna = col;
+        this.Valor = val;
+    }
+
+    public Celula()
+    {
+        this.Direita = null;
+        this.Abaixo = null;  
+        this.Linha = -1;
+        this.Coluna = -1;
+        this.Valor = -1;
+    }
+
+    public Celula Direita { get => direita; set => direita = value; }
+    public Celula Abaixo { get => abaixo; set => abaixo = value; }
+    public int Linha { get => linha; set => linha = value; }
+    public int Coluna { get => coluna; set => coluna = value; }
+    public double Valor { get => valor; set => valor = value; }
+
+    public int CompareTo(Celula other)
+    {
+        return this.Valor.CompareTo(other.Valor);
+    }
+
+    public override string ToString()
+    {
+        return "Direita: " + Direita.ToString() + " Esquerda: " + Abaixo.ToString() + " \n" +
+            "Linha: " + linha + " Coluna: " + coluna + " Valor: " + valor;
+    }
+
+    public Celula LerRegistro(StreamReader arq)
+    {
+
+    }
+}
---
+++ 18196_18204_Projeto1ED/18196_18204_Projeto1ED/ListaLigadaCruzada.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ListaLigadaCruzada
+{
+    Celula noCabeca;
+    int quantosNos;
+
+    bool primeiroAcessoDoPercurso;
+
+    public ListaLigadaCruzada()
+    {
+        NoCabeca = null;
+        quantosNos = 0;
+        primeiroAcessoDoPercurso = false;
+    }
+
+    public Celula NoCabeca { get => noCabeca; set => noCabeca = value; }
+
+    public void PercorrerLista()
+    {
+        for(var i = NoCabeca.Direita; i != null; i = i.Direita)
+        {
+            for(var ii = )
+        }
+    }
+}
